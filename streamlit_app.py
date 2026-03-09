d68155afb0b3efb8856b6d3668574507565889e1
KaveriS28 <125264450+KaveriS28@users.noreply.github.com>
2026-03-09 14:24:40 +0530

    Integrate pandas for DataFrame conversion and display
    Refactor code to use pandas DataFrame for ingredient selection and display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import pandas as pd
 import requests
 
 smoothiefroot_response = requests.get(
@@ -24,8 +24,12 @@ st.write('The name on your smoothie will be: ',name_on_order)
 cnx= st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options")
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pd_df= my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 ingredients_list= st.multiselect('choose up to 5 ingredient:', my_dataframe ,
                                 max_selections=5 )
 if ingredients_list:
@@ -35,6 +39,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
