8d4ce44de9e2c9faf7529b22f6408789c38f4a3e
KaveriS28 <125264450+KaveriS28@users.noreply.github.com>
2026-03-09 14:00:17 +0530

    Fix indentation and formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,10 +36,9 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
+        "https://my.smoothiefroot.com/api/fruit/watermelon")
 
-st.write(smoothiefroot_response.json())
+        st.write(smoothiefroot_response.json())
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
